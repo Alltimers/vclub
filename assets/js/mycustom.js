16b1bfc6dc0eeebb1146fadccd6c3c0d998b630d
Alltimers <53231354+Alltimers@users.noreply.github.com>
2021-07-05 10:28:08 -0300

    commit1

---
+++ assets/js/mycustom.js
@@ -0,0 +1,12 @@
+//Change image
+function changeImage() {
+
+    if (document.getElementById("imgClickAndChange").src = "assets/img/boxout.png") 
+    {
+        document.getElementById("imgClickAndChange").src = "assets/img/boxclosed.png";
+    }
+    else 
+    {
+        document.getElementById("imgClickAndChange").src = "assets/img/boxout.png";
+    }
+}
